8933bbe73bf4b25ee9942ab85ccabeb7cc1727dc
ahqrt <1217223113@qq.com>
2023-11-06 12:16:15 +0800

    feat: 添加测试prisma代码

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { PrismaClient } from "@prisma/client";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const db = new PrismaClient();
+
+const signDTO = t.Object({
+  username: t.String(),
+  password: t.String(),
+});
+
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .post("/sign-up", async ({ body }) =>
+    db.user.create({
+      data: body,
+      select: {
+        id: true,
+        username: true,
+      },
+    }), {
+    error({ code }) {
+      switch (code) {
+        case "P2002":
+          return {
+            code: 409,
+            error: "Username already exists",
+          };
+      }
+    },
+    body: signDTO,
+    response: t.Object({
+      id: t.Number(),
+      username: t.String(),
+    }),
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
